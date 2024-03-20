a8ec006f1ee637a30c584716f8036b2409e0d8ac
Espen Lever <espen.lever@servicewell.se>
2024-03-20 11:54:57 +0100

    first commit

---
+++ DataTypeProject/Program.cs
@@ -0,0 +1,23 @@
+﻿string customerName = "Ms. Barros";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+// Your logic here
+Console.WriteLine($"Dear {customerName},");
+Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\r\n");
+Console.WriteLine($"Currently, you own {currentShares:N} shares at a return of 12.75%.\r\n\r\nOur new product, Glorious Future offers a return of 13.13%.  Given your current volume, your potential profit would be ¤63,000,000.00.\r\n\r\nHere's a quick comparison:\r\n\r\nMagic Yield         12.75%   $55,000,000.00      \r\nGlorious Future     13.13%   $63,000,000.00");
+
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = "";
+
+// Your logic here
+
+Console.WriteLine(comparisonMessage);
---
+++ TestProject/LoopProjects.cs
@@ -0,0 +1,65 @@
+﻿/*string? readResult;
+int numericValue = 0;
+bool validNumber = false;
+bool acceptedValue = false;
+
+do {
+    Console.WriteLine("Enter an integer between 5 and 10:");
+    readResult = Console.ReadLine();
+    validNumber = int.TryParse(readResult, out numericValue);
+    if (readResult != null) {
+        if (validNumber) {
+            if (numericValue >= 5 && numericValue <= 10) {
+                Console.WriteLine($"Your input value ({numericValue}) has been accepted");
+                acceptedValue = true;
+            }
+            else {
+                Console.WriteLine($"You entered {numericValue}. Please enter a number between 5 and 10.");
+            }
+        }
+        else {
+            Console.WriteLine("Sorry, you entered an invalid number, please try again");
+        }
+    }
+
+} while (!acceptedValue);
+*/
+
+/*string? readResult;
+bool acceptedValue = false;
+
+do {
+    Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+    readResult = Console.ReadLine().Trim().ToLower();
+    if (readResult.Contains("administrator") || readResult.Contains("manager") || readResult.Contains("user")) {
+        Console.WriteLine($"Your input value {readResult} has been accepted.");
+        acceptedValue = true;
+    }
+    else {
+        Console.WriteLine($"The role name that you entered, \"{readResult}\" is not valid. Enter your role name (Administrator, Manager, or User)");
+    }
+
+} while (!acceptedValue);*/
+
+using System.Diagnostics.Metrics;
+
+string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+int periodLocation = 0;
+
+for (int i = 0; i < myStrings.Length; i++) {
+    string myString = myStrings[i];
+    periodLocation = myString.IndexOf(".");
+    string mySentence;
+
+    while (periodLocation != -1) {
+        mySentence = myString.Remove(periodLocation);
+        myString = myString.Substring(periodLocation + 1).TrimStart();
+
+        periodLocation = myString.IndexOf(".");
+
+        Console.WriteLine(mySentence);
+    }
+
+    mySentence = myString.Trim();
+    Console.WriteLine(mySentence);
+}
