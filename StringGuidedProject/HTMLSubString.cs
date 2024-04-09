e501e6e1f16898f95d4525026d2f8ad42452c953
espen-servicewell <espen.lever@servicewell.se>
2024-04-09 14:14:50 +0200

    removed and added

---
+++ StringGuidedProject/HTMLSubString.cs
@@ -0,0 +1,20 @@
+﻿const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "";
+string output = "";
+
+string subStringer(string divider, string input)
+{
+    int openingPosition = input.IndexOf($"<{divider}>");
+    int closingPosition = input.IndexOf($"</{divider}>");
+
+    openingPosition += 2 + divider.Length;
+    int length = closingPosition - openingPosition;
+    return input.Substring(openingPosition, length);
+}
+
+quantity = subStringer("span", input);
+output = subStringer("div", input).Replace("trade", "reg");
+
+Console.WriteLine("Quantity: " + quantity);
+Console.WriteLine("Output: " + output);
---
+++ StringGuidedProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StringGuidedProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
